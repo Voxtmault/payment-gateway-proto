839ccef7eb6b9a8e55d8820a1caaa3bcc4f8aeeb
Voxtmault <voxtmault@gmail.com>
2025-05-19 16:07:55 +0700

    feat: removed support for other languages that go feat: moved generated code to root dir

--- generate-code.sh
+++ generate-code.sh
@@ -6,19 +6,7 @@ echo "Generating gRPC Codes"
 export PATH="$PATH:$(go env GOPATH)/bin"
 
 # Generate Go Codes
-protoc --go_out=./go --go-grpc_out=./go snap.proto
-protoc --go_out=./go --go-grpc_out=./go payment-gateway.proto
-protoc --go_out=./go --go-grpc_out=./go management.proto
+protoc --go_out=./ --go-grpc_out=./ snap.proto
+protoc --go_out=./ --go-grpc_out=./ payment-gateway.proto
+protoc --go_out=./ --go-grpc_out=./ management.proto
 echo "Go Codes Generated"
-
-# # Generate JavaScript code
-# protoc --js_out=import_style=commonjs,binary:./js/snap-service snap.proto
-# protoc --js_out=import_style=commonjs,binary:./js/payment-service payment-gateway.proto
-# protoc --js_out=import_style=commonjs,binary:./js/management-service management.proto
-# echo "JS Code Generated"
-
-# # Generate gRPC-Web code
-# protoc --grpc-web_out=import_style=commonjs,mode=grpcwebtext:./web-js/snap-service snap.proto
-# protoc --grpc-web_out=import_style=commonjs,mode=grpcwebtext:./web-js/payment-service payment-gateway.proto
-# protoc --grpc-web_out=import_style=commonjs,mode=grpcwebtext:./web-js/management-service management.proto
-# echo "JS Web Generated"
